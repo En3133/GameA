f8011a39ff4141be0b4dfa0d07059273768daaad
Ian Thornell <84643650+En3133@users.noreply.github.com>
2021-06-25 01:41:11 +0000

    update

--- Game/Main.py
+++ Game/Main.py
@@ -10,18 +10,18 @@ app = QApplication(sys.argv)
 class Game(Scene):
 	def __init__(self):
 		super().__init__(600,600)
-		self.spaceship = Sprite(self, "spaceship100.png", 100, 93)
-		self.spaceship.y=200
+    self.ian = Ian(self)
+    self.iris = Iris(self)
+    self.kelly = Kelly(self)
+    self.amy = Amy(self)
 
+    
 
-		self.spaceship.dx=1
-		
 	def updateGame(self):
-		print("My Update")
-		self.spaceship.x +=1
-		
-		# paint sprites
-		self.spaceship.update()
+      self.ian.update()
+      self.iris.update()
+      self.amy.update()
+      self.kelly.update()
 
 myGame = Game()
 myGame.start()
@@ -30,20 +30,60 @@ sys.exit(app.exec_())
 
 
 class Ian(sprite):
+  def __init__(self, thisScene):
+    super().__init__(thisScene, "sprites/oct.PNG", 100, 100)
+     self.x = 100
+     self.y = 400
+     self.dx 2
+     self.dy -1
+  
+  
+class Arthur(sprite):
   def __init__(self):
-    super.__init__()
-    self.x = 100
-    self.y = 200
-  def move(self):
-    self.x += 1
+    super().__init__(thisScene, "sprites/arthur_sprite.png", 100, 100)
+     self.x = 50
+     self.y = 100
+     self.dx = -1 
+     
+
+class Iris(sprite):
+  def __init__(self, thisScence):
+    super()._init_(thisScence,
+    "sprites/iris_sprite.png", 100, 100)
+    self.x = 50
+    self.y = 100
+    self.dx = 1
+    self.dy = 2
 
 
-class kelly(sprite):
+class Amy(sprite):
   def __init__(self):
-    super.__init__()
-    self.x 
-  def move(self):
-    self.y += 2
+    super().__init__(thisScene, "sprites/amy_sprite.png", 100, 100)
+      self.x = 50
+      self.y = 100
+      self.dy = -1
+
+class Tyrone(sprite):
+def_innit_(self, thisScene)
+  super().__init__(thisScene, "sprites/tryone_sprite.png", 100, 100)
+
+      self.x = 40
+      self.x = 50
+      self.dx = 3
+
+class Kelly(sprite):
+  def__init__(sprite
+    super().__init__(thisScene, "sprites/kelly_sprite.png", 100,100)
+     self.x = 250
+     self.y = 120     
+    self.dx = -2
+    self.dy = -1
+
+
+
+
+
+
 
 
 
