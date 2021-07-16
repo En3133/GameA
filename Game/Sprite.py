a824e0594276b5a05642c4e5b47098bae0b4a9b1
Ian Thornell <84643650+En3133@users.noreply.github.com>
2021-07-16 20:15:32 +0000

    gh

--- Game/Sprite.py
+++ Game/Sprite.py
@@ -13,7 +13,8 @@ class Sprite():
 	DIE = 4
 	CONTINUE = 5
 
-	def __init__(self, thisScene, imageFile, xSize, ySize):self.width = xSize
+  def __init__(self, thisScene, imageFile, xSize, ySize):
+    self.width = xSize
     self.height = ySize
 		self.animation = False
 		self.scene = thisScene
